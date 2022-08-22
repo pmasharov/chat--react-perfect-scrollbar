1bbff81cddef976b8dcd9079278e9e29137b72e6
Pavel Masharov <pmasharov@Pavels-MacBook-Pro.local>
2022-08-22 14:44:34 +0300

    chat messages fetching and scrolling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from 'react'
+import { useEffect, useState } from 'react';
+import ScrollBar from 'react-perfect-scrollbar'
+import styles from './styles.module.scss'
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
-function App() {
+const usePrevious = (value) => {
+  const ref = useRef()
+  useEffect(() => {
+    ref.current = value
+  }, [value])
+  return ref.current
+}
+
+const array = Array(200);
+let data    = [];
+for (let index = 0; index < array.length; index++) {
+  data.push({
+    id:   index,
+    name: `name ${index}`
+  });
+}
+data.reverse()
+
+const fetchItems = (page) => {
+  const onPage = 20;
+  const end    = page * onPage;
+  const start  = end - onPage;
+  return data.slice(start, end);
+};
+
+const deb = (func) => (...params) => {
+  setTimeout(() => {
+    func(...params)
+  }, 200)
+}
+
+export default function App() {
+  const [items, setItems]     = useState([]);
+  const [page, setPage]       = useState(1);
+  const scrollRef             = useRef()
+  const [isFirst, setIsFirst] = useState(true)
+  const [height, setHeight]   = useState(0)
+  const prevHeight            = usePrevious(height)
+
+  useEffect(() => {
+    setItems([
+      ...fetchItems(page)
+        .reverse(),
+      ...items
+    ]);
+  }, [page])
+
+  const onYStart = (container) => {
+    const top = container.scrollHeight - prevHeight
+    container.scrollTo({ top })
+    if (!isFirst && top !== 0) setPage(prev => prev + 1)
+    setIsFirst(false)
+  }
+
+  const onScrollY = (container) => {
+    setHeight(container.scrollHeight)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {items && (
+        <ScrollBar
+          onYReachStart={isFirst ? onYStart : deb(onYStart)}
+          onScrollY={onScrollY}
+          containerRef={container => scrollRef.current = container}
+          className={styles.scroll}
         >
-          Learn React
-        </a>
-      </header>
+          {items.map((item) => (
+            <div className={styles.item} key={item.id}>{item.name}</div>
+          ))}
+        </ScrollBar>
+      )}
     </div>
   );
 }
 
-export default App;
+export { App }
