d246089569f0c6a0774c4262c4a24dc310a37c48
Pavel Masharov <pmasharov@Pavels-MacBook-Pro.local>
2022-08-25 14:42:56 +0300

    implemented with react-infinite-scroll-component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
 import React, { useRef } from 'react'
 import { useEffect, useState } from 'react';
-import ScrollBar from 'react-perfect-scrollbar'
+// import ScrollBar from 'react-perfect-scrollbar'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import styles from './styles.module.scss'
 import 'react-perfect-scrollbar/dist/css/styles.css';
-
-const usePrevious = (value) => {
-  const ref = useRef()
-  useEffect(() => {
-    ref.current = value
-  }, [value])
-  return ref.current
-}
 
 const array = Array(200);
 let data    = [];
@@ -20,62 +13,55 @@ for (let index = 0; index < array.length; index++) {
     name: `name ${index}`
   });
 }
-data.reverse()
 
 const fetchItems = (page) => {
-  const onPage = 20;
+  const onPage = 5;
   const end    = page * onPage;
   const start  = end - onPage;
   return data.slice(start, end);
 };
 
-const deb = (func) => (...params) => {
-  setTimeout(() => {
-    func(...params)
-  }, 200)
-}
-
 export default function App() {
-  const [items, setItems]     = useState([]);
-  const [page, setPage]       = useState(1);
-  const scrollRef             = useRef()
-  const [isFirst, setIsFirst] = useState(true)
-  const [height, setHeight]   = useState(0)
-  const prevHeight            = usePrevious(height)
+  const [items, setItems] = useState([]);
+  const [page, setPage]   = useState(1);
+
+  const fetch = () => {
+    setItems([...items, ...fetchItems(page)])
+  }
+
+  const nextPage = () => {
+    setPage(page + 1)
+  }
+
+  const onNext = () => {
+    fetch()
+    nextPage()
+  }
 
   useEffect(() => {
-    setItems([
-      ...fetchItems(page)
-        .reverse(),
-      ...items
-    ]);
-  }, [page])
-
-  const onYStart = (container) => {
-    const top = container.scrollHeight - prevHeight
-    container.scrollTo({ top })
-    if (!isFirst && top !== 0) setPage(prev => prev + 1)
-    setIsFirst(false)
-  }
-
-  const onScrollY = (container) => {
-    setHeight(container.scrollHeight)
-  }
+    onNext()
+  }, [])
 
   return (
     <div className="App">
-      {items && (
-        <ScrollBar
-          onYReachStart={isFirst ? onYStart : deb(onYStart)}
-          onScrollY={onScrollY}
-          containerRef={container => scrollRef.current = container}
+      <div
+        id="scrollableDiv"
+        className={styles.container}
+      >
+        <InfiniteScroll
+          dataLength={items.length}
+          next={onNext}
+          inverse={true} //
+          hasMore={items.length < data.length}
+          loader={<h4>Loading...</h4>}
           className={styles.scroll}
+          scrollableTarget="scrollableDiv"
         >
           {items.map((item) => (
             <div className={styles.item} key={item.id}>{item.name}</div>
           ))}
-        </ScrollBar>
-      )}
+        </InfiniteScroll>
+      </div>
     </div>
   );
 }
